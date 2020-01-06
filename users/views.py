816265e29f0cc3e83a80cc59e4059e9d4fbb941c
AmosMwangi <amosmwangi101@gmail.com>
2020-01-06 15:22:35 +0300

    made register function

--- users/views.py
+++ users/views.py
@@ -4,3 +4,19 @@ from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from .models import *
 
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(
+                request, f"Your account has been created! You are now able to log in"
+            )
+            return redirect("login")
+    else:
+        form = UserRegisterForm()
+    return render(request, "users/register.html", {"form": form})
+
+
