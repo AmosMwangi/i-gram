916b47a037c870ff5ef8185b989c85a52177594c
AmosMwangi <amosmwangi101@gmail.com>
2020-01-06 15:19:07 +0300

    made some urls patterns

--- insta/urls.py
+++ insta/urls.py
@@ -11,11 +11,7 @@ urlpatterns = [
     path("post/", CreatePostView.as_view(), name="add_post"),
     path("post/<int:pk>/update/", UpdatePostView.as_view(), name="update_post"),
     # path('post/<int:pk>/delete/', DeletePostView.as_view(), name='delete_post'),
-    
-    path("like/", views.like, name="like"),
-    path("follow/", views.follow, name="follow"),
-    path("comment/view/<int:id>/",views.comment_view, name = "view"),
-    path("comment/new/<int:id>/", views.comment, name="comment"),
+
     
 ]
 
